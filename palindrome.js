b5ce5d8f905fd6e3111faa79b675a7aaa1f2cb6f
Junjun Junaedi <79783334+junadiID@users.noreply.github.com>
2021-10-21 11:37:32 +0700

    Add files via upload

---
+++ palindrome.js
@@ -0,0 +1,15 @@
+function reverse(param){
+	var x = param.length, y = '';
+	while(x > 0){
+		y += param[x - 1];
+		x--;
+	}
+	return y;
+}
+var word = "katak";
+if(word === reverse(word)){
+	console.log("Kata `"+word+"`  Palindrome.");
+}
+else{
+	console.log("Kata `"+word+"`   Bukan Palindrome.");
+}
